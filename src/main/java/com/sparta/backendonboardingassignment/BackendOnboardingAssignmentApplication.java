764ee370decae37f3f073f35f75409355ea1bf51
ethrad <asdf123f4f4@gmail.com>
2025-01-17 23:21:18 +0900

    init commit

---
+++ src/main/java/com/sparta/backendonboardingassignment/BackendOnboardingAssignmentApplication.java
@@ -0,0 +1,13 @@
+package com.sparta.backendonboardingassignment;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BackendOnboardingAssignmentApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BackendOnboardingAssignmentApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/sparta/backendonboardingassignment/BackendOnboardingAssignmentApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sparta.backendonboardingassignment;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BackendOnboardingAssignmentApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
