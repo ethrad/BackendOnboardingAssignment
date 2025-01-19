4cbf92600cf30e4f47d0f32a89795fd86dd042fd
ethrad <asdf123f4f4@gmail.com>
2025-01-20 00:47:52 +0900

    Feat: 유저 정보 확인 기능 추가

--- src/main/java/com/sparta/backendonboardingassignment/domain/user/service/UserService.java
+++ src/main/java/com/sparta/backendonboardingassignment/domain/user/service/UserService.java
@@ -1,4 +1,24 @@
 package com.sparta.backendonboardingassignment.domain.user.service;
 
+import org.springframework.stereotype.Service;
+
+import com.sparta.backendonboardingassignment.common.exception.InvalidRequestException;
+import com.sparta.backendonboardingassignment.domain.user.dto.response.UserInfoResponse;
+import com.sparta.backendonboardingassignment.domain.user.entity.User;
+import com.sparta.backendonboardingassignment.domain.user.repository.UserRepository;
+
+import lombok.RequiredArgsConstructor;
+
+@Service
+@RequiredArgsConstructor
 public class UserService {
+
+	private final UserRepository userRepository;
+
+	public UserInfoResponse getUserInfo(String username) {
+		User user = userRepository.findByUsername(username).orElseThrow(
+			() -> new InvalidRequestException("가입되지 않은 유저입니다."));
+
+		return new UserInfoResponse(user.getUsername(), user.getNickname(), user.getAuthorities());
+	}
 }
